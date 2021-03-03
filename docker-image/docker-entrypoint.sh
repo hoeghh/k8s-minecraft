5d775f48fcd258d557890613411bef0a2f52a780
hoeghh <heh@praqma.net>
2021-03-03 14:22:28 +0100

    Updated mc to 1.16.5 and removed VERSION variable

--- docker-image/docker-entrypoint.sh
+++ docker-image/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Running entrypoint script."
+chown -R minecraft /home/minecraft
 
 # Get minecraft jarfile
-echo "Downloading version $VERSION"
-wget -q https://launcher.mojang.com/mc/game/$VERSION/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar -P /home/minecraft/
+echo "Downloading version 1.16.5"
+wget -q https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar -P /home/minecraft/
 
 echo "Accepting eula..."
 echo "# Generated via Docker on $(date)" > /minecraft-data/eula.txt
